c578053140f30ea9b8e50823fd91ded7abac91f1
Nayemja <154550902+Nayemja@users.noreply.github.com>
2024-05-24 12:34:02 +0600

    spacing

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ import 'login.dart';
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: MyLogin(),
+     home: MyLogin(),
     routes: {
       'register': (context) => MyRegister(),
       'login': (context) => MyLogin(),
